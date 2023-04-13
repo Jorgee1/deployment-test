0c58dbc83b7b6caaf572e790c7f022cbbbde1918
Jorgee1 <21368470+Jorgee1@users.noreply.github.com>
2023-04-13 08:29:07 -0500

    init

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import Fastify from "fastify"
+
+
+const fastify = Fastify()
+
+fastify.get('/test', () => ({hey: 'response'}))
+
+const main = async () => {
+    await fastify.listen({ port:3000 })
+}
+main()
