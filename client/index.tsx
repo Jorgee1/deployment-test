850922651563ecd1eee4f4a2b4f703e137106b90
Jorgee1 <21368470+Jorgee1@users.noreply.github.com>
2023-04-13 15:26:31 -0500

    Added trpc and cors with react

--- client/index.tsx
+++ client/index.tsx
@@ -1,30 +1,60 @@
+import { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+import { createTRPCReact, httpBatchLink } from '@trpc/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { AppRouter } from '../server/trpc'
+
+const trpc = createTRPCReact<AppRouter>()
+
+const Nav = () => <nav>
+    <Link to='/'>Home</Link>
+    <Link to='/test'>Test</Link>
+</nav>
+
+const Home = () => {
+    const { data, isLoading, isError } = trpc.greeting.useQuery()
+
+    if (!data) {
+        if (isLoading) return <>Loading...</>
+        if (isError) return <>Error</>
+    }
+
+    return <>
+        <div>Home</div>
+        <Nav/>
+        <div>{data}</div>
+    </>
+}
+
+const Test = () => {
+    return <>
+        <div>Test</div>
+        <Nav/>
+    </>
+}
 
 const App = () => {
-    const Nav = () => <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/test'>Test</Link>
-    </nav>
+    const url = new URL(document.URL)
+    const [trpcClient] = useState(() => trpc.createClient({links: [httpBatchLink({url: `http://${url.hostname}:3000/trpc`})]}))
+    const [queryClient] = useState(() => new QueryClient())
 
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <>
-                <div>Home</div>
-                <Nav/>
-
-            </>
+            element: <Home/>
         },
         {
             path: '/test',
-            element: <>
-                <div>Test</div>
-                <Nav/>
-            </>
+            element: <Test/>
         }
     ])
-    return <RouterProvider router={router}/>
+
+    return <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}/>
+        </QueryClientProvider>
+    </trpc.Provider>
 }
 
 
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,19 @@
 import Fastify from "fastify"
+import { fastifyCors } from '@fastify/cors'
+import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify'
+import { appRouter, AppRouter } from './trpc'
 
 
 const fastify = Fastify()
+fastify.register(fastifyTRPCPlugin<AppRouter>, {
+    prefix: '/trpc',
+    trpcOptions: {
+        router: appRouter
+    }
+})
+fastify.register(fastifyCors, {
+    origin: true
+})
 
 fastify.get('/test', () => ({ hey: 'response' }))
 
