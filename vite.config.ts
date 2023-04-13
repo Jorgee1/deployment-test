cc554b853727d2469149aba7b331739ae9259be4
Jorgee1 <21368470+Jorgee1@users.noreply.github.com>
2023-04-13 09:21:44 -0500

    Added enviromental variable to deploy directly

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 
+
 export default defineConfig({
     build: {
-        outDir: 'public',
+        outDir: process.env.PUBLIC_DIR || 'public',
         emptyOutDir: true
     }
 })
